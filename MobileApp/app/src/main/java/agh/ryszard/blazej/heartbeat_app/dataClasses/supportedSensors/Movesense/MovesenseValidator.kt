ba7fb50e9011bf2616fc1399240a7f91095a851b
Ryszard Błażej <sapermysza@gmail.com>
2024-06-23 21:09:16 +0200

    New branch init

--- MobileApp/app/src/main/java/agh/ryszard/blazej/heartbeat_app/dataClasses/supportedSensors/Movesense/MovesenseValidator.kt
+++
@@ -1,8 +0,0 @@
-package agh.ryszard.blazej.heartbeat_app.dataClasses.supportedSensors.Movesense
-
-import agh.ryszard.blazej.heartbeat_app.dataClasses.jsonSerializables.BtSensor
-
-fun movesenseValidator(device: BtSensor): Boolean{
-    val regex = Regex("Movesense")
-    return regex.containsMatchIn(device.name)
-}
